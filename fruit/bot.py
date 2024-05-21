d8590afdb12155cd256c5a704ea304e1d9897159
Asilbek-cyber <159028877+Asilbek-cyber@users.noreply.github.com>
2024-05-21 18:20:20 +0500

    Update bot.py

--- fruit/bot.py
+++ fruit/bot.py
@@ -1,7 +1,7 @@
 import requests
 
 def send_message(text):
-    url = f"https://api.telegram.org/bot6705729790:AAGfYghjIzmTNu5_bUkDeogvaC0Aw08fjbg/sendMessage"
-    params = {"chat_id": '1776373061', "text": text}
+    url = f"https://api.telegram.org/bot6961503071:AAGDxwQKafQZS89CtHAbiIOc2Ces-wckukk/sendMessage"
+    params = {"chat_id": '882056073', "text": text}
     response = requests.post(url, data=params)
     return response.json()
